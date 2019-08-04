aacf3a9c82c2375dadc7d378731f661e7a633052
Ashok <ashok91choudhary@gmail.com>
2019-08-04 19:58:28 -0400

    added udated scripts

--- scripts/Memory_Mass_Vs_Spin_v3.py
+++ scripts/Memory_Mass_Vs_Spin_v3.py
@@ -6,9 +6,9 @@ from matplotlib.font_manager import FontProperties
 from matplotlib import colors, cm
 from matplotlib.ticker import LogLocator
 from matplotlib.colors import LogNorm
-import plotsettings
-from matplotlib.font_manager import FontProperties
 
+#Set memory growth rate
+grwth = 5.0*pow(10, -5)
 
 def find_nearest_idx(array, value):
     array = np.asarray(array)
@@ -28,8 +28,6 @@ def s(t, hpmem, mu, phi):
 
 	return st
 
-# Set rate of memory growth
-grwth = 5.0*pow(10, -5)
 
 # Function that compute memory growth during two weeks interval of time for a given mass
 
@@ -69,56 +67,53 @@ def Memory_growth_in_two_weeks(log_SolarMass, Spin):
 		datafile_hNR='rMPsi4_Sz1_'+filename+'_Sz2_'+filename+'_q1p5dataN4Clean_hNR.dat'
 		timeNR, hp, hc = np.loadtxt(file_location_hmem+datafile_hNR, unpack=True)
 
+
 	hmem_diff = np.zeros(len(hmem))
 	for i in range(len(hmem)):
 		hmem_diff[i] =  (hmem[-i]-hmem[-(i+1)])/abs(hmem[-i])
 
 	
-	time_final = timeNR[-find_nearest_idx(hmem_diff, grwth)]
+	time_final = timeNR[-find_nearest_idx(hmem_diff, grwth)]	
 
 	t_initial =  time_initial(log_SolarMass, time_final)
 
 	idx = find_nearest_idx(timeNR, t_initial)
 
-	hmem_two_weeks = hmem[idx:find_nearest_idx(timeNR, time_final)]
-	time_two_weeks = timeNR[idx:find_nearest_idx(timeNR, time_final)]
+#	print timeNR[np.argmax(hp)]	
+	hmem_diff = abs(hmem[find_nearest_idx(timeNR, time_final)]-hmem[idx])
 	
+#	plt.plot(timeNR, hp)
+#	plt.show()
 
-	return timeNR, hmem, time_two_weeks, hmem_two_weeks
+	return hmem_diff
 
 
 # Make heatmap for the two weeks memory growth in SMBHB		
-#Spin_array = np.array([-0.901, -0.601, -0.431, -0.201, 0.0 ,0.201, 0.601, 0.801, 0.99])
+Spin_array = np.array([-0.941, -0.801, -0.601, -0.431, -0.201, 0.0 ,0.201, 0.601, 0.801, 0.99])
+Mass_array = np.linspace(8, 9.5, len(Spin_array))
 
-legend_size = 2
-fig = plt.figure()
-fontP = FontProperties()
-fontP.set_size('20.')
+Memory_growth_M_vs_Spin = np.zeros([len(Spin_array), len(Spin_array)])
 
-Spin_array = np.array([-0.941, 0.0, 0.99])
+#4.4*(10**(Mass_array[j]-23))
+for i in range(len(Spin_array)):
+	for j in range(len(Mass_array)):
+		Memory_growth_M_vs_Spin[i][j]  = 4.6*(10**(Mass_array[j]-23))*Memory_growth_in_two_weeks(Mass_array[j], Spin_array[i])
 
-Mass_array = np.array([8.0, 8.5, 9.0,  9.5, 10.0])
-
-shift = 2500
-k=0
-for i in Spin_array:
-	for j in Mass_array:
-			timeNR = Memory_growth_in_two_weeks(j, i)[0]
-			hmem = Memory_growth_in_two_weeks(j, i)[1]
-			time_two_weeks = Memory_growth_in_two_weeks(j, i)[2]
-			hmem_two_weeks = Memory_growth_in_two_weeks(j, i)[3]
-
-			plt.plot(timeNR+k*shift, hmem,'k--')
-			plt.plot(time_two_weeks+k*shift, hmem_two_weeks, label='M='+str(round(j,1))+' \t\t S ='+ str(round(i, 2)))
-			k+=1		
-
-#plt.xlim(-3500, 4500)
-plt.xlabel(r'$t/M$')
-plt.ylabel(r'$(R/M)\,h^{(mem)}_{+}$')
-plt.legend(loc=2)
-fontP.set_size('12.')
+fig, ax = plt.subplots()
+im = plt.imshow(Memory_growth_M_vs_Spin, interpolation='bilinear', cmap=cm.RdYlGn, origin='lower', extent=[-0.94, 0.99, 8, 9.5], norm=LogNorm(vmin=9.511959855858058e-21, vmax=1.0311708502287188e-17))
+plt.xlabel('Spin')
+plt.ylabel('Log{(M)')
+plt.clim(abs(Memory_growth_M_vs_Spin).min(),abs(Memory_growth_M_vs_Spin).max())
+plt.colorbar()
+fig.tight_layout()
+#plt.savefig("/home/ashok/Desktop/gravitational_wave_memory_project/plots/Memory_Spin_vs_Mass.pdf")	
 plt.show()
 
+
+#print abs(Memory_growth_M_vs_Spin).min(),abs(Memory_growth_M_vs_Spin).max()
+print Memory_growth_M_vs_Spin.min(), Memory_growth_M_vs_Spin.max()
+
+# Compute RMS reseduals as function of Spin Vs Mass
 
 def compute_rms_reseduals(log_SolarMass, Spin): 
 
@@ -155,7 +150,7 @@ def compute_rms_reseduals(log_SolarMass, Spin):
 
 		datafile_hNR='rMPsi4_Sz1_'+filename+'_Sz2_'+filename+'_q1p5dataN4Clean_hNR.dat'
 		timeNR, hp, hc = np.loadtxt(file_location_hmem+datafile_hNR, unpack=True)
-
+	
 	hmem_diff = np.zeros(len(hmem))
 	for i in range(len(hmem)):
 		hmem_diff[i] =  (hmem[-i]-hmem[-(i+1)])/abs(hmem[-i])
@@ -167,55 +162,41 @@ def compute_rms_reseduals(log_SolarMass, Spin):
 
 	idx = find_nearest_idx(timeNR, t_initial)
 
-	
-	#hmem_two_weeks =  4.4*(10**(log_SolarMass-23))*hmem[idx:find_nearest_idx(timeNR, time_final)]
-	hmem_two_weeks =  hmem[idx:find_nearest_idx(timeNR, time_final)]
-	#hmem = 4.4*(10**(log_SolarMass-23))*hmem
-	timeNR_two_weeks = timeNR[idx:find_nearest_idx(timeNR, time_final)]
+	hmem_two_weeks =  hmem[idx:find_nearest_idx(timeNR, grwth)]
+	timeNR_two_weeks = timeNR[idx:find_nearest_idx(timeNR, grwth)]
 
 	#compute the quadratic fit and subtact it from reseduals
 	res = s(timeNR_two_weeks, hmem_two_weeks, 0, 0) 
 	res_quadfit = np.polyfit(timeNR_two_weeks, res, 2)
 	res1d = np.poly1d(res_quadfit)
-	quadratic_fit_to_res = res1d(timeNR_two_weeks)
-	res_quadSubtract = res-quadratic_fit_to_res
+	res_quadSubtract = res-res1d(timeNR_two_weeks)
 
 	
-	#mean of reseduals
-	res_mean = np.sqrt(np.mean(res_quadSubtract**2))
+	# mean of reseduals
+	res_mean = 4.6*(10**(log_SolarMass-23))*np.sqrt(np.mean(res_quadSubtract**2))
 
-	timeNR_two_weeks = 4.6*pow(10, log_SolarMass-11)*timeNR_two_weeks
-
-	return timeNR_two_weeks, res, quadratic_fit_to_res, res_quadSubtract, res_mean
+#	plt.semilogx(timeNR, h_mem_plus)
+#	plt.semilogx(timeNR_two_weeks, hmem_two_weeks, 'k--')
+#	plt.plot(timeNR_two_weeks, res_quadSubtract, 'k--')
+#	plt.show()
+	
+	return res_mean
       
-legend_size = 2
-fig = plt.figure()
-fontP = FontProperties()
-fontP.set_size('20.')
+# Make heatmap for the two weeks memory growth in SMBHB		
 
-shift = 50
-k=0
-time_last=0
-for i in Spin_array:
-	for j in Mass_array:
-			timeNR_two_weeks = compute_rms_reseduals(j, i)[0]		
-			res_two_weeks = compute_rms_reseduals(j, i)[1]
-			res_quad_fit_two_weeks = compute_rms_reseduals(j, i)[2]
-			res_quadSubtract_two_weeks = compute_rms_reseduals(j, i)[3]
+Reseduals_M_vs_Spin = np.zeros([len(Spin_array), len(Spin_array)])
 
-			timeNR_two_weeks = timeNR_two_weeks - timeNR_two_weeks[0] +  k*shift
-			plt.plot(timeNR_two_weeks, res_two_weeks,'k--')
-			plt.plot(timeNR_two_weeks, res_quad_fit_two_weeks ,'r--')
-			plt.plot(timeNR_two_weeks, res_quadSubtract_two_weeks , label='M='+str(round(j,1))+' \t\t S ='+ str(round(i, 2)))
-			#time_last = timeNR_two_weeks[-1]+25
+for i in range(len(Spin_array)):
+	for j in range(len(Mass_array)):
+		Reseduals_M_vs_Spin[i][j]  = compute_rms_reseduals(Mass_array[i], Spin_array[j])
 
-			k+=1		
-
-plt.ylim(-0.02, 0.2)
-plt.xlabel(r'$days$')
-plt.ylabel(r'$(R/M)\,h^{(mem)}_{+}$')
-plt.legend(loc=2)
-fontP.set_size('12.')
+fig, ax = plt.subplots()
+im = plt.imshow(Reseduals_M_vs_Spin , interpolation='bilinear', cmap=cm.RdYlGn, origin='lower', extent=[-0.94, 0.99, 8, 9.5], norm=LogNorm(vmin=7.4011072070563e-19, vmax=4.152466994869437e-17))
+plt.xlabel('Spin')
+plt.ylabel('Log{(M)')
+plt.colorbar()
+fig.tight_layout()
+#plt.savefig("/home/ashok/Desktop/gravitational_wave_memory_project/plots/MemoryRes_Spin_vs_Mass.pdf")
 plt.show()
 
-
+print Reseduals_M_vs_Spin.min(), Reseduals_M_vs_Spin.max()	
